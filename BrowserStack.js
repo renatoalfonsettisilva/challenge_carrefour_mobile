69434ad9f8346ec2eccd96fc93ebafe5a6258350
Renato Alfonsetti Silva <46387936+renatoalfonsettisilva@users.noreply.github.com>
2026-03-11 16:35:34 -0300

    Create BrowserStack_iOS.js

---
+++ BrowserStack.js
@@ -0,0 +1,14 @@
+capabilities: [
+    {
+        'bstack:options': {
+            projectName: 'My Project',
+            sessionName: 'My Session',
+            key: 'YOUR_BROWSERSTACK_KEY',
+            username: 'YOUR_BROWSERSTACK_USERNAME'
+        },
+        device: 'iPhone 16',
+        os: 'ios',
+        os_version: '26',
+        app: 'bs://<app-id>'
+    }
+],
