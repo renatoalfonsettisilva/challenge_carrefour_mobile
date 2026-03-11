da90ff360e01bb6167268731e8ca6dd6380e3558
Renato Alfonsetti Silva <46387936+renatoalfonsettisilva@users.noreply.github.com>
2026-03-11 16:34:29 -0300

    Create wdio.conf.js

---
+++ wdio.conf.js
@@ -0,0 +1,14 @@
+capabilities: [
+    {
+        platformName: 'Android',
+        deviceName: 'Android Emulator',
+        app: '/path/to/app.apk',
+        automationName: 'UiAutomator2'
+    },
+    {
+        platformName: 'iOS',
+        deviceName: 'iPhone Simulator',
+        app: '/path/to/app.app',
+        automationName: 'XCUITest'
+    }
+],
