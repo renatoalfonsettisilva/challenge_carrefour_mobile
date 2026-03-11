df1364960ad0caf4ffda59c76a2415c3744bee88
Renato Alfonsetti Silva <46387936+renatoalfonsettisilva@users.noreply.github.com>
2026-03-11 16:37:19 -0300

    Create BrowserStack_Android.js

---
+++ BrowserStack_Android.js
@@ -0,0 +1,14 @@
+capabilities: [
+    {
+        'bstack:options': {
+            projectName: 'My Project',
+            sessionName: 'My Session',
+            key: 'YOUR_BROWSERSTACK_KEY',
+            username: 'YOUR_BROWSERSTACK_USERNAME'
+        },
+        device: 'Sansung a56',
+        os: 'ios',
+        os_version: '17',
+        app: 'bs://<app-id>'
+    }
+],
